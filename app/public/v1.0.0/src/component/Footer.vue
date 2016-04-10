7a51e734d2ee92dfe03d2050f5227ad713816955
glassysky <873661157@qq.com>
2016-04-11 00:01:39 +0800

    footer component styles

--- app/public/v1.0.0/src/component/Footer.vue
+++ app/public/v1.0.0/src/component/Footer.vue
@@ -5,13 +5,16 @@
                 <div class="contact-way">
                     <h3>联系方式:</h3>
                     <ul>
-                        <li><i class="fa fa-github"></i></li>
-                        <li></li>
-                        <li></li>
+                        <li><a href=""><i class="fa fa-github fa-fw fa-2x"></i></a></li>
+                        <li><a href=""><i class="fa fa-qq fa-fw fa-2x"></i></a></li>
+                        <li><a href=""><i class="fa fa-envelope fa-fw fa-2x"></i></a></li>
+                        <li><a href=""><i class="fa fa-twitter fa-fw fa-2x"></i></a></li>
                     </ul>
                 </div>
                 <div class="contact-address">
                     <h3>位置:</h3>
+                    <p>南京邮电大学(仙林校区)</p>
+                    <p>教四-304/314</p>
                 </div>
             </div>
             <div class="footer-block friend-link">
@@ -41,6 +44,7 @@
         width: 100%;
         min-width: 1000px;
         height: $footer-part-height;
+        padding: 30px 20px;
         background-color: $footer-part-background;
         color: #aaa;
 
@@ -51,12 +55,48 @@
 
             .footer-block {
                 height: 100%;
-                padding: 30px 20px;
+                box-sizing: border-box;
+                padding: 20px 20px;
+                
+                h3 {
+                    color: #aaa;
+                }
             }
             
             .contact {
                 float: left;
                 width: $side-part-width;
+
+                .contact-way {
+
+                    h3 {
+
+                    }
+                    ul {
+                        overflow: hidden;
+                        margin: 20px 0;
+
+                        li {
+                            float: left;
+                            margin: 0 10px;
+
+                            a {
+                                i {
+                                    color: #fff;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                .contact-address {
+                    p {
+                        width: 100%;
+                        margin: 20px 0;
+                        text-indent: 2em;
+                        font-size: 0.8em;
+                    }
+                }
             }
 
             .friend-link {
@@ -66,6 +106,8 @@
 
             .qr-code {
                 margin: 0 $side-part-width - 2;
+                border-right: 1px solid #333;
+                border-left: 1px solid #333;
             }
         }
 
