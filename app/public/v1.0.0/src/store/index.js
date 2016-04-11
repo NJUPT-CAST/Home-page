2efa09ac594668be8555595255dbba9f79f12cb3
glassysky <873661157@qq.com>
2016-04-12 01:04:45 +0800

    add store & use vuex

---
+++ app/public/v1.0.0/src/store/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by cuitianhao on 16/4/12.
+ */
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+    state: {
+
+    },
+    actions: {
+
+    },
+    mutations: {
+        
+    }
+})
