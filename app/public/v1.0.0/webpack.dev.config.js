6136c58229b1fc085922203484184a2238578675
glassysky <873661157@qq.com>
2016-05-07 20:40:26 +0800

    banner

--- app/public/v1.0.0/webpack.dev.config.js
+++ app/public/v1.0.0/webpack.dev.config.js
@@ -4,34 +4,38 @@ var path = require('path');
 module.exports = {
     entry: {
         index: [
-            path.resolve(__dirname, "src", "entry", "index.js"),
             'webpack-dev-server/client?http://localhost:8000',
             'webpack/hot/only-dev-server',
+            path.resolve(__dirname, "./src/entry/index.js")
         ]
     },
     output: {
         path: path.join(__dirname, "dist"),
         filename: "[name].bundle.js",
-        publicPath: path.join(__dirname, "public", "javascripts")
+        publicPath: '/static/'
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
     module: {
         loaders: [
             {
-                test: /\.scss$/,
-                loaders: "style!css!sass"
+                test: /\.css$/,
+                loader: "style!css"
             },
             {
-                test: /\.jsx?$/,
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test: /\.js?$/,
                 loaders: ['react-hot', 'jsx?harmony'],
                 include: path.join(__dirname, 'src')
             }
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
+    }
 }
