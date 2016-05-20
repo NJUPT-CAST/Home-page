6adbad5497796dbf74a0daa8e859c299499f175f
glassysky <873661157@qq.com>
2016-05-20 08:58:49 +0800

    set router rules

--- app/src/main.js
+++ app/src/main.js
@@ -2,15 +2,23 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 
+import Home from './components/Home'
 import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 
 Vue.use(VueRouter)
 
 var router = new VueRouter()
 
 router.map({
+  '/': {
+    component: Home
+  },
   '/signin': {
     component: SignIn
+  },
+  '/signup': {
+    component: SignUp
   }
 })
 
