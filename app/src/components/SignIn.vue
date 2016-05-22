66f15dceae8621507d6cbc9615d5deddeb36bbbb
glassysky <873661157@qq.com>
2016-05-22 15:47:49 +0800

    copy into signin component

--- app/src/components/SignIn.vue
+++ app/src/components/SignIn.vue
@@ -1,19 +1,54 @@
 <template ejs="ejs">
-<h1>{{ msg }}</h1>
+  <div class="sign-in-container panel-container">
+    <div id="sign-in-panel" class="form-panel">
+      <div class="panel-head">
+        <h2>登录</h2>
+      </div>
+      <div class="panel-body">
+        <my-form
+          :form-data="formData"
+        ></my-form>
+      </div>
+    </div>
+    <div id="right-section">
+
+    </div>
+  </div>
 </template>
 
 <script>
+import Form from './Form.vue'
+
 export default {
   data () {
     return {
-      msg: 'this is SignIn component'
+      formData: [
+        {
+          name: '学号',
+          idName: 'stu-id',
+          type: 'text'
+        },
+        {
+          name: '密码',
+          idName: 'password',
+          type: 'password'
+        },
+        {
+          isBtn: true,
+          btnName: '登录',
+          idName: 'sign-in-btn',
+          type: 'button'
+        }
+      ]
     }
+  },
+  components: {
+    'my-form': Form
   }
 }
 </script>
 
-<style lang="scss">
-h1 {
-  color: red;
-}
+<style lang="scss" scoped>
+@import '../assets/signPanel.scss';
+
 </style>
--- app/src/components/SignUp.vue
+++ app/src/components/SignUp.vue
@@ -1,6 +1,6 @@
 <template ejs="ejs">
-  <div class="sign-up-container">
-    <div id="sign-up-panel">
+  <div class="sign-up-container panel-container">
+    <div id="sign-up-panel" class="form-panel">
       <div class="panel-head">
         <h2>注册</h2>
       </div>
@@ -44,65 +44,23 @@ export default {
           type: 'password'
         },
         {
-          name: '验证码',
-          idName: 'code',
-          type: 'text'
-        },
-        {
           isBtn: true,
           btnName: '注册',
           idName: 'sign-up-btn',
           type: 'button'
         }
-      ],
-      msg: 'this is SignUp component'
+      ]
     }
   },
   components: {
     'my-form': Form
+  },
+  methods: {
   }
 }
 </script>
 
-<style lang="scss">
-
-.sign-up-container {
-  display: flex;
-  width: 1000px;
-  min-width: 500px;
-  padding: 30px;
-  margin: 0 auto;
-
-  #sign-up-panel {
-    flex: 7;
-    border-radius: 2px;
-    border: 1px solid #aaa;
-    overflow: hidden;
-    margin-right: 20px;
-
-    .panel-head {
-      width: 100%;
-      border-bottom: 1px solid #eee;
-      h2 {
-        margin: 0;
-        padding: 10px;
-        font-size: 1em;
-        color: #999;
-      }
-    }
-
-    .panel-body {
-      padding: 20px 50px 20px 0;
-    }
-
-  }
-
-  #right-section {
-    flex: 3;
-    width: 300px;
-    border: 1px solid #aaa;
-    border-radius: 2px;
-  }
-}
+<style lang="scss" scoped>
+@import '../assets/signPanel.scss';
 
 </style>
